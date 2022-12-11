fc910beb131d49d5b42b4126cdaa0c6cb434bdd7
BernardoKlopf <bernardo.klopffleisch@contabilizei.com.br>
2022-12-11 19:52:49 -0300

    optmizing imports

--- src/main/kotlin/com/app/weather/rest/WeatherRest.kt
+++ src/main/kotlin/com/app/weather/rest/WeatherRest.kt
@@ -1,11 +1,7 @@
 package com.app.weather.rest
 
 import com.app.weather.service.ForecastService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("weather")
