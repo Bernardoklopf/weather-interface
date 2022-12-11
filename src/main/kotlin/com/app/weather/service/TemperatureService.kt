8f37ad97d4f4a2fbb58128c24477f451a0890b54
BernardoKlopf <bernardo.klopffleisch@contabilizei.com.br>
2022-12-11 19:53:17 -0300

    adding logs

--- src/main/kotlin/com/app/weather/service/TemperatureService.kt
+++ src/main/kotlin/com/app/weather/service/TemperatureService.kt
@@ -6,6 +6,8 @@ import com.app.weather.entity.Temperature
 import com.app.weather.repository.TemperatureRepository
 import com.app.weather.util.extensions.*
 import com.app.weather.vo.ForecastVo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,14 +16,18 @@ class TemperatureService(
     private val openWeatherForecastComponent: OpenWeatherForecastComponent
 ) {
 
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     fun findOrRequestTemperatureForTomorrow(city: City) = getTemperatureForTomorrow(city)
         ?: saveForecastAsTemperature(openWeatherForecastComponent.getForecast(city))
             .filter {
                 it.date.belongsToTomorrow()
-            }
+            }.also { logger.debug("Temperature for tomorrow doesn't exist. Resquesting for city $city") }
 
     fun findOrRequestTemperatureNextFiveDays(city: City) = getTemperatureForNextFiveDays(city)
-        ?: saveForecastAsTemperature(openWeatherForecastComponent.getForecast(city))
+        ?: saveForecastAsTemperature(openWeatherForecastComponent.getForecast(city)).also {
+            logger.debug("Temperature for next five full days doesn't exist. Resquesting for city $city")
+        }
 
     private fun getTemperatureForTomorrow(city: City): List<Temperature>? = temperatureRepository
         .findAllByCityAndDateBetween(
@@ -29,6 +35,9 @@ class TemperatureService(
             tomorrow().first.toDate(),
             tomorrow().second.toDate()
         ).ifEmpty { null }
+        .also {
+            logger.debug("Finding temperature for tomorrow for city id = $city")
+        }
 
     private fun getTemperatureForNextFiveDays(city: City): List<Temperature>? = temperatureRepository
         .findAllByCityAndDateBetween(
@@ -38,7 +47,10 @@ class TemperatureService(
         ).filter {
             it.date.belongsToFiveDaysFromNow()
         }.ifEmpty { null }
+        .also {
+            logger.debug("Finding temperature for next five days for city id = $city")
+        }
 
-    private fun saveForecastAsTemperature(forecast: ForecastVo): List<Temperature> = temperatureRepository
-        .saveAll(forecast.toListTemperature()).toList()
+    private fun saveForecastAsTemperature(forecast: ForecastVo): List<Temperature> =
+        temperatureRepository.saveAll(forecast.toListTemperature()).toList()
 }
